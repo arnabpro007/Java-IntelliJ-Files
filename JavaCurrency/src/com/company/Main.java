949ea4d5e1f1d6d0db241440937ee42ffbe0401b
bhuto1998 <arnab1998tyt17@gmail.com>
2020-08-16 17:55:58 +0530

    reg-up

--- JavaCurrency/src/com/company/Main.java
+++ JavaCurrency/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.text.FieldPosition;
 import java.text.NumberFormat;
+import java.text.ParsePosition;
 import java.util.Currency;
 import java.util.Locale;
 import java.util.Scanner;
@@ -12,8 +14,31 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     double payment = scanner.nextDouble();
     scanner.close();
-    NumberFormat nF = NumberFormat.getCurrencyInstance();
-    nF.setCurrency(Currency.getInstance(Locale.CANADA));
-    System.out.println(nF.getCurrency().getDisplayName());
+    NumberFormat numberFormat = new NumberFormat() {
+        @Override
+        public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+            return null;
+        }
+
+        @Override
+        public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+            return null;
+        }
+
+        @Override
+        public Number parse(String source, ParsePosition parsePosition) {
+            return null;
+        }
+    };
+    Currency currency_us = Currency.getInstance("USD");
+    Currency currency_india = Currency.getInstance("INR");
+    Currency currency_china = Currency.getInstance("CNY");
+    Currency currency_france = Currency.getInstance("EUR");
+        System.out.println("US: " + currency_us.getSymbol() + payment);
+        System.out.println("India: " + currency_india.getSymbol() + payment);
+        System.out.println("China: " + currency_china.getSymbol() + payment);
+        System.out.println("France: " + payment + " " +currency_france.getSymbol());
+
+
     }
 }
